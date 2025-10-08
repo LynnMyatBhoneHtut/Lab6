f9910130590305de36b44d27419791bd16cdb77f
Lynn Myat Bhone Htut <lynnmyatbhonehtut@Lynns-MacBook-Pro.local>
2025-10-08 13:27:16 +0800

    Resolved merge conflict in login.php

--- login.php
+++ login.php
@@ -1,25 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-<?php include('header.inc'); ?>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form method="post" action="process.php"> <label for="username">Username:</label>
-<input type="text" name="username" required><br>
-<label for="password">Password:</label >
-<input type="password" name="password" required><br>
-<input type="hidden" name="token" value="abc123">
-<input type="submit" value="Login">
-</form>
-<?php include('footer.inc'); ?>
-</body>
-</html>
-=======
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -41,4 +21,3 @@
     <?php include('footer.inc'); ?>
 </body>
 </html>
->>>>>>> 5d22ed7 (Updated LAB 6-1 files)
